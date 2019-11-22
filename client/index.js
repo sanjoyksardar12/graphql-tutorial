9066894e2324f7dcc5ecf5f1acbbccf22e40e713
sanjoyk <sanjoyksardar01@gmail.com>
2019-11-22 17:14:54 +0530

    section 9.c showing a particular song and fetching individual records

--- client/index.js
+++ client/index.js
@@ -8,6 +8,7 @@ import { Router, Route, hashHistory, IndexRoute } from "react-router";
 import App from "./components/App.js";
 import SongList from "./components/SongList";
 import SongCreate from "./components/songCreate";
+import SongDetail from "./components/songDetail";
 
 const client = new ApolloClient({});
 
@@ -17,6 +18,7 @@ const Root = () => {
       <Route path="/" component={App}>
         <IndexRoute component={SongList} />
         <Route path="songs/new" component={SongCreate} />
+        <Route path="songs/:id" component={SongDetail} />
       </Route>
     </Router>
   </ApolloProvider>)
