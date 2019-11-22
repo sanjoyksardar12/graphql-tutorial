7ae771ac13dddb072efb9db0d5c804d62525f8a4
sanjoyk <sanjoyksardar01@gmail.com>
2019-11-22 17:32:52 +0530

    section 9.d create lyric creation form

---
+++ client/components/LyricCreate.js
@@ -0,0 +1,13 @@
+import React, { Component} from "react";
+
+class LyricCreate extends Component {
+  render() {
+    return (
+      <form>
+        <label>Add a lyric</label>
+        <input></input>
+      </form>
+    )
+  }
+}
+export default LyricCreate;
